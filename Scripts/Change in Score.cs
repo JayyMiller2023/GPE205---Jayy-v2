348b65c5ef55143402f3de787f4cf92b596fa0e2
Jayy_Mil49 <JMILLER86022@uat.edu>
2023-11-12 21:18:03 -0700

    Final Update
    So Long Class!

---
+++ Scripts/Change in Score.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeinScore : MonoBehaviour
+{
+    public Text Changeinscore;
+
+    public Controller controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Changeinscore.text = controller.Score;
+    }
+}
